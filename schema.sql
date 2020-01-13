1e16cf731ba6ec51618131404b53b8f4116cfd93
Alex Happe <abhdiver1994@gmail.com>
2020-01-13 15:47:50 -0600

    adds in the schema for the database

---
+++ schema.sql
@@ -0,0 +1,31 @@
+-- Drops the day_planner_db if it already exists --
+DROP DATABASE IF EXISTS employee_db;
+
+-- Create the database day_planner_db and specified it for use.
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+-- Create the table plans.
+CREATE TABLE department (
+  id int NOT NULL AUTO_INCREMENT,
+  name varchar(30) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+id INT NOT NULL AUTO_INCREMENT,
+title varchar(30) NOT NULL,
+salary DECIMAL,
+department_id INT,
+PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+id INT NOT NULL AUTO_INCREMENT,
+first_name VARCHAR (30) NOT NULL,
+last_name VARCHAR (30) NOT NULL,
+role_id INT,
+manager_id INT,
+PRIMARY KEY (id)
+);
